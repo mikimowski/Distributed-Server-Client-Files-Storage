9742a4d6bccc1dff6eb49f29ce176dc3d83f089b
Maciek <mikula.maciek@gmail.com>
2019-06-02 20:40:27 +0200

    clean up

--- run_client.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-./netstore-client -g 239.10.11.12 -p 40000 -o ./client1_storage/
--- run_server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-./netstore-server -g 239.10.11.12 -p 40000 -f ./server1_storage/
